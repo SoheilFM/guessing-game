49e770cebaf67ce22fe5030f0caacf32f332f277
SoheilFM <soheil.farid1999@yahoo.com>
2023-04-29 01:38:47 +0000

    limit to the number of guesses

--- guessing-game.py
+++ guessing-game.py
@@ -3,11 +3,14 @@ import random
 # Generate a random number between 1 and 100
 number = random.randint(1, 100)
 
+# Set the maximum number of guesses
+max_guesses = 10
+
 # Keep track of the number of guesses
 guesses = 0
 
-# Loop until the player guesses the correct number
-while True:
+# Loop until the player guesses the correct number or runs out of guesses
+while guesses < max_guesses:
     # Get a guess from the player
     guess = int(input("Guess a number between 1 and 100: "))
     guesses += 1
@@ -20,3 +23,7 @@ while True:
         print("Too low, try again.")
     else:
         print("Too high, try again.")
+
+# If the player runs out of guesses, display the correct number
+if guesses == max_guesses:
+    print("Sorry, you ran out of guesses. The number was", number)
